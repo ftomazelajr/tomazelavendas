dc9e9e2123f589a8a293437b7f2571d29af7d727
ftomazelajr <tomazelapiscinas@gmail.com>
2025-04-24 15:35:42 -0300

    Update server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.post('/criar-pagamento', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
